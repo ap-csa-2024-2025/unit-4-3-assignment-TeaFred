cd6d40c8811ab63ed49f35e7b3b69241a9f7e8f3
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-11-15 01:57:54 +0000

    asdasd

--- Main.java
+++ Main.java
@@ -4,6 +4,19 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    //Problem 1
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Type in an integer: ");
+    int num1 = 1;
+    System.out.println("Type in an integer greater than the previous: ");
+    int num2 = 25;
+
+    for (int i = 0; i <= 25; i++)
+    {
+      if (i % 2 == 1)
+      {
+        System.out.println(i);
+      }
+    }
   }
 }
